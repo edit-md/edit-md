1fc4cfccf4acea5cb628881ec0cfe68d21fb0c74
Westphal, Oliver <oli-west@outlook.de>
2024-12-30 23:04:03 +0100

    Removed minio_url from .env; added authentication to controller methodes; added util class from document-service to file-service

--- services/file-service/src/main/kotlin/md/edit/services/file/controllers/FileController.kt
+++ services/file-service/src/main/kotlin/md/edit/services/file/controllers/FileController.kt
@@ -1,5 +1,7 @@
 package md.edit.services.file.controller
 
+import md.edit.services.file.utils.AuthorizationUtils
+import org.springframework.security.core.Authentication
 import md.edit.services.file.service.FileService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -14,14 +16,16 @@ class FileController(private val fileService: FileService) {
 
     @PostMapping("/upload")
     @Throws(IOException::class, NoSuchAlgorithmException::class, InvalidKeyException::class)
-    fun uploadFile(@RequestParam("file") file: MultipartFile): ResponseEntity<String> {
+    fun uploadFile(@RequestParam("file") file: MultipartFile, authentication: Authentication): ResponseEntity<String> {
+        //val user = AuthorizationUtils.onlyUser(authentication) // Need to look over the configuration classes
         val fileName = fileService.uploadFile(file)
         return ResponseEntity.ok("File uploaded: $fileName")
     }
 
     @GetMapping("/download/{fileName}")
     @Throws(IOException::class)
-    fun downloadFile(@PathVariable fileName: String): ResponseEntity<ByteArray> {
+    fun downloadFile(@PathVariable fileName: String, authentication: Authentication): ResponseEntity<ByteArray> {
+        //val user = AuthorizationUtils.onlyUser(authentication) // Need to look over the configuration classes
         val inputStream = fileService.downloadFile(fileName)
         val content = inputStream.readAllBytes()
         return ResponseEntity.ok(content)
