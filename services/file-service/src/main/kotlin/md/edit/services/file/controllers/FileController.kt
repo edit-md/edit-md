2a25de25dc67eb08a846ebf1af578e514913ddf9
Palm, Eric <erpa1002@stud.hs-kl.de>
2025-01-03 21:56:42 +0100

    File Service: updated default value of minio.bucket-name in application.properties; created new test-endpoint for uploading a file via the presigned url

--- services/file-service/src/main/kotlin/md/edit/services/file/controllers/FileController.kt
+++ services/file-service/src/main/kotlin/md/edit/services/file/controllers/FileController.kt
@@ -1,38 +1,66 @@
 package md.edit.services.file.controllers
 
 import md.edit.services.file.dtos.FileDtoOut
-import md.edit.services.file.utils.AuthorizationUtils
-import org.springframework.security.core.Authentication
 import md.edit.services.file.services.FileService
+import md.edit.services.file.utils.AuthorizationUtils
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.security.core.Authentication
 import org.springframework.web.bind.annotation.*
-import org.springframework.web.multipart.MultipartFile
 import org.springframework.web.server.ResponseStatusException
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileWriter
 import java.io.IOException
-import java.security.InvalidKeyException
-import java.security.NoSuchAlgorithmException
+import java.net.HttpURLConnection
+import java.net.URI
+import java.net.URL
 import java.util.*
 
+
 @RestController
-@RequestMapping
 class FileController(private val fileService: FileService) {
 
+    // Endpoint only for testing purposes
     @PostMapping("/upload")
-    @Throws(IOException::class, NoSuchAlgorithmException::class, InvalidKeyException::class)
-    fun uploadFile(@RequestParam("file") file: MultipartFile, authentication: Authentication): ResponseEntity<String> {
-        val user = AuthorizationUtils.onlyUser(authentication)
-        val fileName = fileService.uploadFile(file)
-        return ResponseEntity.ok("File uploaded: $fileName")
-    }
+    fun uploadFile(): ResponseEntity<String> {
 
-    @GetMapping("/download/{fileName}")
-    @Throws(IOException::class)
-    fun downloadFile(@PathVariable fileName: String, authentication: Authentication): ResponseEntity<ByteArray> {
-        val user = AuthorizationUtils.onlyUser(authentication)
-        val inputStream = fileService.downloadFile(fileName)
-        val content = inputStream.readAllBytes()
-        return ResponseEntity.ok(content)
+        // Make new File and URL
+        val presignedUrl : String = fileService.generatePresignedUploadUrl()
+        val file: File = File("example.txt")
+        file.createNewFile()
+        val url: URL = URI(presignedUrl).toURL()
+
+        // Write something in the file
+        try {
+            val writer: FileWriter = FileWriter(file)
+
+            writer.write("I was born on the cemetery\n")
+            writer.write("Under the sign of the moon")
+
+            writer.close()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        // Upload the file
+        val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
+        connection.setDoOutput(true)
+        connection.setRequestMethod("PUT")
+        connection.setRequestProperty("Content-Type", "application/octet-stream")
+
+        connection.getOutputStream().use { outputStream ->
+            FileInputStream(file).use { fileInputStream ->
+                val buffer = ByteArray(8192)
+                var bytesRead: Int
+                while ((fileInputStream.read(buffer).also { bytesRead = it }) != -1) {
+                    outputStream.write(buffer, 0, bytesRead)
+                }
+            }
+        }
+
+        val responseCode: Int = connection.getResponseCode()
+        return ResponseEntity.ok("Response Code: $responseCode")
     }
 
     @GetMapping("/{fileId}/download")
